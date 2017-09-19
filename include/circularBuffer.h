b84fe0d0178f2a49c92e17a43858f03fc9d89824
Christoph Diegelmann <christoph.diegelmann@gmx.de>
2017-09-19 21:23:33 +0200

    Update circularBuffer.h

--- include/circularBuffer.h
+++ include/circularBuffer.h
@@ -11,38 +11,40 @@ class circularBuffer {
         this->head_ = this->buffer_.begin();  // after the end
         this->tail_ = this->buffer_.begin();  // first element
     }
-	
-	T& front() {
-		return *this->tail_;
-	}
+    
+    T& front() {
+      return *this->tail_;
+    }
 
     void pop_front() {
-	  if (this->size() > 0) {
-		this->tail_++;
-		if(this->tail_ == this->buffer_.end())
-		  this->tail_ = this->buffer_.begin();
-	  }
+      if (this->size() > 0) {
+        this->tail_++;
+        if(this->tail_ == this->buffer_.end())
+          this->tail_ = this->buffer_.begin();
+      }
     }
 
     bool push_back(T element) {
-	  if (this->size() < S - 1) {
-		*this->head_++ = element;
-		if (this->head_ == this->buffer_.end())
-		  this->head_ = this->buffer_.begin();
-	  }
-	  return false;
+      if (this->size() == S)
+		this->pop_front();
+	  
+      *this->head_++ = element;
+	  if (this->head_ == this->buffer_.end())
+	    this->head_ = this->buffer_.begin();
+
+      return false;
     }
 
     std::size_t size() {
-	  auto size = std::distance(this->tail_, this->head_);
-	  if (size < 0)
-	    size += this->buffer_.size();
+      auto size = std::distance(this->tail_, this->head_);
+      if (size < 0)
+        size += this->buffer_.size();
       return size;
     }
     
  private:
 
-    typedef std::array<T, S> StorageClass;
+    typedef std::array<T, S + 1> StorageClass;
 
     StorageClass buffer_;
     typename StorageClass::iterator head_;
